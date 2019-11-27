f14522524cca2891529b0bd7a7155eb201b6ca89
Qawelesizwe Mlilo <qawemlilo@gmail.com>
2019-11-27 13:51:25 +0200

    Updates

---
+++ precache-manifest.5a88e1b8b3cf9fd41e32324e449950ae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebbbcceb7bfacc5a4fb7",
+    "url": "/m-teto/css/app.c73c4853.css"
+  },
+  {
+    "revision": "fff40ffef9161028fd1d",
+    "url": "/m-teto/css/chunk-vendors.ac58e55d.css"
+  },
+  {
+    "revision": "2b3a40b326ec2c2193e1e261f7f05fc5",
+    "url": "/m-teto/img/bg.2b3a40b3.png"
+  },
+  {
+    "revision": "4c0ba15269b214acea129c432e339549",
+    "url": "/m-teto/img/logo.4c0ba152.png"
+  },
+  {
+    "revision": "55e6a9030410b426e7c4dbfff45e4cfc",
+    "url": "/m-teto/img/teto.55e6a903.gif"
+  },
+  {
+    "revision": "471c76080b677e4f87e9b0501612fdc7",
+    "url": "/m-teto/index.html"
+  },
+  {
+    "revision": "ebbbcceb7bfacc5a4fb7",
+    "url": "/m-teto/js/app.d53b0341.js"
+  },
+  {
+    "revision": "fff40ffef9161028fd1d",
+    "url": "/m-teto/js/chunk-vendors.903139ed.js"
+  },
+  {
+    "revision": "bea4295f4752e15090c11508bc3adaa6",
+    "url": "/m-teto/manifest.json"
+  },
+  {
+    "revision": "d4caaffc1e8c0545744551e8549f1c28",
+    "url": "/m-teto/media/teto-intro.d4caaffc.mp3"
+  },
+  {
+    "revision": "0dbb14a209b3e90c1071b54c8eb43054",
+    "url": "/m-teto/media/teto.0dbb14a2.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/m-teto/robots.txt"
+  }
+]);
