0a72c46960c0838a73210146fb754fc9058f2188
Qawelesizwe Mlilo <qawemlilo@gmail.com>
2019-11-27 13:50:40 +0200

    ...

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,21 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: process.env.NODE_ENV === 'production' ? '/m-teto/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/m-teto/' : '/',
+
+  pwa: {
+    name: 'M-TETO',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    iconPaths: {
+      favicon32: 'img/icons/23.png',
+      favicon16: 'img/icons/16.png',
+      appleTouchIcon: 'img/icons/152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/144.png'
+    }
+  }
 }
